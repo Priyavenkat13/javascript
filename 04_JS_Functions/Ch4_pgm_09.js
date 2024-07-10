7e81462595f5aee6d729b205b3c20d82c26d903b
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 18:56:28 +0530

    completed

--- 04_JS_Functions/Ch4_pgm_09.js
+++ 04_JS_Functions/Ch4_pgm_09.js
@@ -1,17 +1,20 @@
 // Using a function to display object properties
-
 var showMovieInfo;
 
 showMovieInfo = function () {
-  console.log("Movie information for " + movie.title);
-  console.log("------------------------------");
-  console.log("Actors: " + movie.actors);
-  console.log("Directors: " + movie.directors);
-  console.log("------------------------------");
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
 };
 
-
-
+var movie;
+movie = {};
+movie.title = "Inception";
+movie.actors = "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page";
+movie.directors = "Christopher Nolan";
+showMovieInfo();
 /* Further Adventures
  *
  * 1) Add a call to showMovieInfo and run the program.
