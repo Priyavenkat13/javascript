c4a3d9456457e76e19ed5371320aa7e2d8e3b315
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 15:25:58 +0530

    completed

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -2,7 +2,7 @@
 
 var book1;
 var book2;
-
+var book3;
 book1 = {
 	title: "The Hobbit",
 	author: "J. R. R. Tolkien"
@@ -13,8 +13,15 @@ book2 = {
 	author: "Philip Pullman"
 };
 
+
+book3 = {
+	title: "Northern Lights",
+	author: "k.k.Rowling"
+};
+
 console.log(book1.title + " by " + book1.author);
 console.log(book2.title + " by " + book2.author);
+console.log(book2.title + " by " + book3.author);
 
 
 
