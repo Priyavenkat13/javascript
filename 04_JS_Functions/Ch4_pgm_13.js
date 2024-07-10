73def7374bcaa063dac30d2044ae0d98f830ae3b
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 19:16:38 +0530

    completed

--- 04_JS_Functions/Ch4_pgm_13.js
+++ 04_JS_Functions/Ch4_pgm_13.js
@@ -4,6 +4,8 @@ var player1;
 var player2;
 var player;
 var showPlayerInfo;
+var showPlayerPlace;
+var showPlayerHealth;
 
 player1 = {
     name: "Kandra",
@@ -17,6 +19,12 @@ player2 = {
     health: 40
 };
 
+showPlayerPlace = function () {
+    console.log(player.name + " is in " + player.place);
+};
+showPlayerHealth = function () {
+    console.log(player.name + " has health " + player.health);
+};
 showPlayerInfo = function () {
     console.log(player.name);
     console.log("------------------------------");
