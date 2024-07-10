27733aab762e97003c24833576c04d5c1d1bf2a7
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 20:27:42 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_09.js
+++ 05_JS_Arguments/Ch5_pgm_09.js
@@ -3,11 +3,17 @@
 var showPlayerHealth;
 
 showPlayerHealth = function (playerName, playerHealth) {
-    console.log(playerName + " has health " + playerHealth);
+    var healthInfo;
+    healthInfo = playerName + ": health " + playerHealth;
+    console.log(healthInfo);
 };
 
 showPlayerHealth("Kandra", 50);
 showPlayerHealth("Dax", 40);
+
+// Call the showPlayerHealth function with your own arguments
+showPlayerHealth("Aria", 60);
+showPlayerHealth("Bram", 70);
 
 
 
