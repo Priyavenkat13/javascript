036981f98d39c92995af183038daa4ae8fe6aba1
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 15:38:47 +0530

    completed

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -7,7 +7,15 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
-
+console.log(post);
+var post2 = {
+	id : 1,
+	title : "My Crazy Space Adventure",
+	author : "Philae",
+	created : "2000-06-21",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+console.log(post2);
 
 
 /* Further Adventures
