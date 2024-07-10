027dd3c1079d807ed06729fbda3938a8786f67f5
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 15:35:36 +0530

    completed

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -13,6 +13,19 @@ player1.score = player1.score + 50;
 
 console.log(player1.name + " has scored " + player1.score);
 
+var player1;
+
+player2= {
+	  name: "Max",
+	  score: 10
+};
+
+console.log(player2.name + " has scored " + player2.score);
+
+player2.score = player2.score + 50;
+
+console.log(player2.name + " has scored " + player2.score);
+
 
 
 /* Further Adventures
