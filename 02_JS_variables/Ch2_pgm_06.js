437c59dd4fe0f041e63801f06bd7c79af6412423
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-10 14:24:03 +0530

    completed

--- 02_JS_variables/Ch2_pgm_06.js
+++ 02_JS_variables/Ch2_pgm_06.js
@@ -2,13 +2,12 @@
 
 var playerName;
 var locationName;
-
+var score;
 playerName = "Lionel Messi";
 locationName = "Argentina";
-
+score ="100";
 console.log(playerName + " is in " + locationName);
-
-
+console.log(playerName+ " has a score of "+ score );
 
 /* Further Adventures
  *
