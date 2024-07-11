b3d83253c67f6738303c110df78749348e606010
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-11 15:34:51 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_02.js
+++ 07_JS_Object_Arguments/Ch7_pgm_02.js
@@ -4,6 +4,7 @@ var planet1 = { name: "Jupiter", radius: 69911 };
 
 var calculateSizes = function (planet) {
     var r = planet.radius;
+    planet.diameter =2*r;
     planet.area = 4 * 3.142 * r * r;
     planet.volume = 4 * 3.142 * r * r * r / 3;
 };
@@ -12,6 +13,7 @@ var displaySizes = function (planet) {
     console.log(planet.name);
     console.log("surface area = " + planet.area + " square km");
     console.log("volume = " + planet.volume + " cubic km");
+    console.log("diameter = " + planet.diameter + " km");
 };
 
 calculateSizes(planet1);
