a43c5ba16d0041d438cde4e53b1738f880a6d181
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-11 21:05:58 +0530

    completed

--- 09_JS_Constructors/Ch9_pgm_07.js
+++ 09_JS_Constructors/Ch9_pgm_07.js
@@ -21,16 +21,33 @@ var question1 = new QuizQuestion(
     "What is the capital of France?",
     "Paris"
 );
+var question2 = new QuizQuestion(
+    "random access memory",
+    "RAM"
+);
+var question3 = new QuizQuestion(
+    "What is the largest planet in our solar system?",
+    "Jupiter"
+);
 
 question1.addOption("Bordeaux");
 question1.addOption("F");
 question1.addOption("Paris");
 question1.addOption("Brussels");
-
+question1.addOption("BrK");
 question1.showQuestion();
 
+question2.addOption("ROM");
+question2.addOption("CPU");
+question2.addOption("EPROM");
+question2.addOption("MM");
+question2.showQuestion();
 
-
+question3.addOption("Earth");
+question3.addOption("Mars");
+question3.addOption("Jupiter");
+question3.addOption("Saturn");
+question3.showQuestion();
 /* Further Adventures
  *
  * 1) Add a fifth option to question1.
