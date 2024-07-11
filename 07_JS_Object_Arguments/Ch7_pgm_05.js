8732c79c740ed9a54d3f86a435f317c3f5347df3
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-11 15:59:44 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -7,10 +7,26 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
+var showLarger = function (num1, num2) {
+    var smaller = Math.min(num1, num2);
+    var larger = Math.max(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
+var showSmallest = function (num1, num2, num3) {
+    var smallest = Math.min(num1, num2, num3);
+
+    console.log("The smallest number is " + smallest);
+};
+
 showSmaller(12, 3);
 showSmaller(-10, 3);
 
-
+showLarger(12, 3);
+showLarger(20, 13);
+showSmallest(12, 3, 9);s
+showSmallest(-10, 3, -5);
 
 /* Further Adventures
  *
