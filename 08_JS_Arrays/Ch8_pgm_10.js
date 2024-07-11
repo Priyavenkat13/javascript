4d242f23647794893df49655eb0bde5233c739d8
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-11 19:10:35 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_10.js
+++ 08_JS_Arrays/Ch8_pgm_10.js
@@ -1,5 +1,22 @@
 // Using the arguments passed by forEach - compact
 
+var rectangles = [
+  { length: 10, width: 3 },
+  { length: 10, width: 6 }
+];
+var assignArea = function(rectangle) {
+  rectangle.area = rectangle.length * rectangle.width;
+};
+var showInfo = function(rectangle) {
+  console.log("Rectangle - Length: " + rectangle.length + ", Width: " + rectangle.width + ", Area: " + rectangle.area);
+};
+
+rectangles.forEach(function(rectangle, index, array) {
+  assignArea(rectangle);
+  showInfo(rectangle);
+});
+
+
 [ "Dax", "Jahver", "Kandra" ].forEach(function (item, index, wholeArray) {
     console.log("Item: " + item);
     console.log("Index: " + index);
