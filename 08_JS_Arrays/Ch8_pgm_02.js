1db03bde01fd83edcd324f61d9884ec63613a908
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-11 18:30:10 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_02.js
+++ 08_JS_Arrays/Ch8_pgm_02.js
@@ -5,12 +5,12 @@ var place2 = { name : "The Grand Canyon", country : "USA" };
 var place3 = { name : "Bondi Beach", country : "Australia" };
 
 var thisYear = [ place1, place2 ];
-var nextYear = [ place3 ];
+var nextYear = [ place3 ,place1];
 
 console.log(thisYear);
 console.log(nextYear);
 
-
+console.log("Next Year after adding place1:", nextYear);
 
 /* Further Adventures
  *
