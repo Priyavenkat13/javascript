079bd984231137853b5e40133ce5c86bceb6e56f
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-12 15:23:53 +0530

    completed

--- 10_JS_Conditions/Ch10_pgm_05.js
+++ 10_JS_Conditions/Ch10_pgm_05.js
@@ -4,10 +4,10 @@ var getGuesser = function () {
     var secret = Math.floor(Math.random() * 10 + 1);
   
     return function (userNumber) {
-      if (userNumber === secret) {
+      if (userNumber< secret) {
         return "Well done!";
       } else {
-        if (userNumber < secret) {
+        if (userNumber === secret) {
           return "Too low!";
         } else {
           return "Too  high!";
@@ -15,11 +15,14 @@ var getGuesser = function () {
       }
     };
   };
+
   
-  var guess = getGuesser();
-  console.log(guess());
+
+
   
-  
+var guess = getGuesser();
+ console.log(guess());
+console.log(guess(2));
   /* Further Adventures
    *
    * 1) Run the program
