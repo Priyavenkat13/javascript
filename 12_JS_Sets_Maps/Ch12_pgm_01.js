9dda753c3704771c2406e95be522285f4b35c212
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-12 19:51:43 +0530

    completed

--- 12_JS_Sets_Maps/Ch12_pgm_01.js
+++ 12_JS_Sets_Maps/Ch12_pgm_01.js
@@ -1,7 +1,29 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
 
+const emptySet = new Set();
+console.log('Empty Set:', emptySet);
+const numberSet = new Set();
+for (let i = 0; i <= 10; i++) {
+    numberSet.add(i);
+}
+console.log('Set containing numbers 0 to 10:', numberSet);
+
+numberSet.delete(5); 
+console.log('Set after removing 5:', numberSet);
+
+numberSet.clear();
+console.log('Set after clearing:', numberSet);
+const stringArray = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+const stringSet = new Set(stringArray);
+console.log('Set of 5 string elements:', stringSet);
+const countries = ['Finland', 'Sweden', 'Norway'];
+const countryMap = new Map();
+for (const country of countries) {
+    countryMap.set(country, country.length);
+}
+console.log('Map of countries and number of characters:');
+for (const [country, length] of countryMap) {
+    console.log(`${country}: ${length}`);
+}
 
 // create an empty set
 // Create a set containing 0 to 10 using loop
