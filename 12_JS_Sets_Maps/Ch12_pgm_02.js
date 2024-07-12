a679d1a59631433895fb1b09b6e8a6f913499c20
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-12 20:31:19 +0530

    completed

--- 12_JS_Sets_Maps/Ch12_pgm_02.js
+++ 12_JS_Sets_Maps/Ch12_pgm_02.js
@@ -1,7 +1,14 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
 
 // Find a union b
 // Find a intersection b
-// Find a with b
+// Find a with b    
+
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+const countries = ['Finland', 'Sweden', 'Norway'];
+const union = new Set([a]);
+console.log('Union-', Array.from(union));
+const intersection = new Set(a.filter(num => b.includes(num)));
+console.log('Intersection-', Array.from(intersection));
+const difference = new Set(a.filter(num => !b.includes(num)));
+console.log('Difference (a - b)-', Array.from(difference));
