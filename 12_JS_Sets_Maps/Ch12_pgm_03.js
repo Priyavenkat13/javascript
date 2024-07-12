d62878780257843a218eff90477a62340e411f33
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-12 20:36:46 +0530

    completed

--- 12_JS_Sets_Maps/Ch12_pgm_03.js
+++ 12_JS_Sets_Maps/Ch12_pgm_03.js
@@ -1,11 +1,8 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+
 
 // How many languages are there in the countries object file.
 
 // *** Use the countries data to find the 10 most spoken languages:
-
 
 //    // Your output should look like this
 //    console.log(mostSpokenLanguages(countries, 10))
@@ -30,3 +27,37 @@ const countries = ['Finland', 'Sweden', 'Norway']
 //   {French:45},
 //   {Arabic:25}
 //   ]
+const countriesData = [
+    { name: 'Finland', languages: ['Finnish', 'Swedish'] },
+    { name: 'Sweden', languages: ['Swedish'] },
+    { name: 'Norway', languages: ['Norwegian'] },
+    { name: 'USA', languages: ['English'] },
+    { name: 'Canada', languages: ['English', 'French'] },
+    
+  ];
+  
+  const mostSpokenLanguages = (countries, topN) => {
+    const languageCount = {};
+  
+    countries.forEach(country => {
+      country.languages.forEach(language => {
+        if (languageCount[language]) {
+          languageCount[language]++;
+        } else {
+          languageCount[language] = 1;
+        }
+      });
+    });
+  
+    const sortedLanguages = Object.entries(languageCount)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, topN)
+      .map(([language, count]) => ({ [language]: count }));
+  
+    return sortedLanguages;
+  };
+  
+  console.log(mostSpokenLanguages(countriesData, 10));
+  console.log(mostSpokenLanguages(countriesData, 3));
+  
+
