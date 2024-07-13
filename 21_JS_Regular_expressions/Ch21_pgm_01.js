fbe76f421c1eb0fc0cfc34a450047f570416cacb
Priyavenkat13 <priyadharshini84814@gmail.com>
2024-07-13 09:56:58 +0530

    completed

--- 21_JS_Regular_expressions/Ch21_pgm_01.js
+++ 21_JS_Regular_expressions/Ch21_pgm_01.js
@@ -4,3 +4,14 @@
 // is_valid_variable('first-name') # False
 // is_valid_variable('1first_name') # False
 // is_valid_variable('firstname') # True
+
+
+
+function is_valid_variable(variable){
+    const validVariablePattrne=/^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
+    return validVariablePattrne.test(variable);
+}
+console.log(is_valid_variable('first_name'));
+console.log(is_valid_variable('first-name')); 
+console.log(is_valid_variable('1first_name')); 
+console.log(is_valid_variable('firstname')); 
